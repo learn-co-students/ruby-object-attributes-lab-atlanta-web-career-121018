d07d650eca4ff70f1b743416c8da78aa2c0f1bb1
Chanda Grayson <chandagrayson@gmail.com>
2019-01-30 16:44:14 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,20 @@
 class Dog
 
+  attr_reader :name, :breed
+
+  # def initialize(name, breed)
+  #   @name = name
+  #   @breed = breed
+  # end
+
   def name=(name)
     @name = name
-  end
-
-  def name
-    @name
   end
 
   def breed=(breed)
     @breed = breed
   end
 
-  def breed
-    @breed
-  end
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,13 @@
 class Person
 
+  attr_reader :name, :job
+
   def name=(name)
     @name = name
-  end
-
-  def name
-    @name
   end
 
   def job=(job)
     @job = job
   end
-
-  def job
-    @job
-  end
-
 
 end
